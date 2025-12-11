1d305adc42adb387fb86a6dc1ca3a19c0ccda945
lynaten <13523142@std.stei.itb.ac.id>
2025-12-11 18:52:14 +0700

    feat: sender detect timeout

--- src/server.rs
+++ src/server.rs
@@ -1,32 +1,35 @@
-use tonic::{transport::Server, Request, Response, Status};
-use tokio::runtime::Runtime;
-use tokio::time::{sleep, Duration};
+use std::env;
+// use test::test_client::TestClient;
 use test::test_server::{Test, TestServer};
 use test::{TestReply, TestRequest};
-use test::test_client::TestClient;
-use std::env;
+use tokio::runtime::Runtime;
+use tokio::sync::watch;
+use tokio::time::timeout;
+use tokio::time::{sleep, Duration};
+use tonic::{transport::Server, Request, Response, Status};
 
 pub mod test {
     tonic::include_proto!("test");
 }
 
-#[derive(Debug, Default)]
-pub struct MyTest {}
+#[derive(Debug)]
+pub struct MyTest {
+    tx: tokio::sync::watch::Sender<i32>,
+}
 
 #[derive(Debug, Default)]
 pub struct Timeout {}
 
 #[tonic::async_trait]
 impl Test for MyTest {
-    async fn request(
-        &self,
-        request: Request<TestRequest>,
-    ) -> Result<Response<TestReply>, Status> {
-        println!("Got a request: {:?}", request);
-
+    async fn request(&self, request: Request<TestRequest>) -> Result<Response<TestReply>, Status> {
+        // println!("Got a request: {:?}", request);
         let reply = TestReply {
             message: format!("Hello {}!", request.into_inner().name),
         };
+
+        sleep(Duration::from_millis(100)).await;
+        let _ = self.tx.send(1);
 
         Ok(Response::new(reply))
     }
@@ -61,54 +64,50 @@ impl Test for MyTest {
 //     pub fn initialize(&mut self, port: &String) {
 //         println!{"TODO: initialize with port: {port}"};
 //     }
-//     pub fn 
+//     pub fn
 // }
 
-
-async fn receiver() -> Result<(), Box<dyn std::error::Error>> {
+async fn receiver(tx: tokio::sync::watch::Sender<i32>) -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1]; 
+    let query = &args[1];
     let mut addr_str = String::from("[::1]:");
     addr_str.push_str(query);
     let addr = addr_str.parse()?;
-    let test = MyTest::default();
+    let test = MyTest { tx: tx };
 
     Server::builder()
         .add_service(TestServer::new(test))
         .serve(addr)
         .await?;
 
-
     Ok(())
 }
 
-async fn sender() -> Result<(), Box<dyn std::error::Error>> {
-    for _ in 0..3 {
-        sleep(Duration::from_millis(1000)).await;
-        let args: Vec<String> = env::args().collect();
-        let query = &args[1];
-        let mut addr_str = String::from("http://[::1]:");
-        addr_str.push_str(query); 
-        let mut client = TestClient::connect(addr_str).await?;
-        let request = tonic::Request::new(TestRequest {
-            name: "Hello".into(),
-        });
-
-        let response = client.request(request).await?;
-
-        println!("RESPONSE={:?}", response);
+async fn sender(
+    rx: &mut tokio::sync::watch::Receiver<i32>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    loop {
+        match timeout(Duration::from_millis(5000), rx.changed()).await {
+            Ok(_) => println!("not time out yayyy"),
+            Err(_) => println!("time out brow"),
+        }
     }
-
-    Ok(())
 }
-
 
 fn main() {
     let rt = Runtime::new().unwrap();
+    let (tx, mut rx) = watch::channel::<i32>(0);
+
+    print!("{:?}\n", tx);
     rt.block_on(async move {
         println!("hello from the async block");
-        tokio::spawn(async { let _ = receiver().await; });
-        tokio::spawn(async { let _ = sender().await; });
+
+        tokio::spawn(async {
+            let _ = receiver(tx).await;
+        });
+        tokio::spawn(async move {
+            let _ = sender(&mut rx).await;
+        });
     });
     loop {}
 }
